9fa001dd9cb9e20448af2290789bb4642f5f5574
afristorm-3 <afristorm3@gmail.com>
2023-08-17 18:04:02 -0400

    fix auth task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,7 +102,7 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
 
-   def update_password(self, reset_token: str, password: str) -> None:
+    def update_password(self, reset_token: str, password: str) -> None:
         """Updates a user's password given the user's reset token.
         """
         user = None
